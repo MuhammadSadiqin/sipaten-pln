a4b46335e1bea2fd681e5ba143d7331e6019c9a1
MuhammadSadiqin <msadiqin46@gmail.com>
2023-07-10 11:50:58 +0700

    baru

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('roles')->default('ADMIN');
+            $table->string('level')->default('ADMIN');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();

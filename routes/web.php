41aa0fde491553b14d21a33feacd27381bd98348
MuhammadSadiqin <msadiqin46@gmail.com>
2023-07-14 16:57:23 +0700

    baru web

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-use App\Models\GantiMeter;
-use Illuminate\Support\Facades\Route;
-=======
-use App\Models\Amr;
-use App\Models\GantiMeter;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AmrController;
 use App\Http\Controllers\LbkbController;
@@ -27,32 +21,29 @@ use App\Models\TigaPhasa;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+        return view('welcome');
 });
 
 // Route::get('users-export', UserController::class)->name('users.export');
 
 
 Route::prefix('dashboard')
-    ->middleware(['auth:sanctum', 'admin'])
-    ->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('amr-export', [AmrController::class, 'export'])->name('amr.export');
-        Route::get('tigaphasa-export', [TigaPhasaController::class, 'export'])->name('tigaphasa.export');
-        Route::get('gantimeter-export', [GantiMeterController::class, 'export'])->name('gantimeter.export');
-        Route::get('lbkb-export', [LbkbController::class, 'export'])->name('Lbkb.export');
-        Route::post('users-import', [UserController::class, 'import'])->name('users.import');
-        Route::post('GantiMeter-import', [GantiMeterController::class, 'import'])->name('GantiMeter.import');
-<<<<<<< HEAD
+        ->middleware(['auth:sanctum', 'admin'])
+        ->group(function () {
+                Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+                Route::get('amr-export', [AmrController::class, 'export'])->name('amr.export');
+                Route::get('tigaphasa-export', [TigaPhasaController::class, 'export'])->name('tigaphasa.export');
+                Route::get('gantimeter-export', [GantiMeterController::class, 'export'])->name('gantimeter.export');
+                Route::get('lbkb-export', [LbkbController::class, 'export'])->name('Lbkb.export');
+                Route::post('users-import', [UserController::class, 'import'])->name('users.import');
+                Route::post('GantiMeter-import', [GantiMeterController::class, 'import'])->name('GantiMeter.import');
 
 
-=======
-        Route::post('lbkb-import', [LbkbController::class, 'import'])->name('Lbkb.import');
-        Route::post('Amr-import', [AmrController::class, 'import'])->name('Amr.import');
->>>>>>> a7bd448abfda22c83961d8765be2a2c188f50ded
-        Route::resource('users', UserController::class);
-        Route::resource('gantimeter', GantiMeterController::class);
-        Route::resource('tigaphasa', TigaPhasaController::class);
-        Route::resource('lbkb', LbkbController::class);
-        Route::resource('amr', AmrController::class);
-    });
+                Route::post('lbkb-import', [LbkbController::class, 'import'])->name('Lbkb.import');
+                Route::post('Amr-import', [AmrController::class, 'import'])->name('Amr.import');
+                Route::resource('users', UserController::class);
+                Route::resource('gantimeter', GantiMeterController::class);
+                Route::resource('tigaphasa', TigaPhasaController::class);
+                Route::resource('lbkb', LbkbController::class);
+                Route::resource('amr', AmrController::class);
+        });

3f15bee48c316d0ac102d9d62e6339cbcdfd596b
MuhammadSadiqin <msadiqin46@gmail.com>
2023-07-14 16:55:11 +0700

    slicing admin template

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -42,7 +42,7 @@
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                                 Email
                             </label>
-                            <input value="{{ old('email') ?? $item->email }}" name="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="email" placeholder="User Email">
+                            <input value="{{ $item->email }}" name="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="email" placeholder="User Email">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
